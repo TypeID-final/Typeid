5f7a62f98b7afddb9f8329851a352cd550460325
Asna Abdul Kareem <145547693+Asnaabdul@users.noreply.github.com>
2025-12-05 11:35:17 +0530

    Delete finalyrproject directory

--- finalyrproject/ml/src/model.py
+++
@@ -1,9 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-
-def make_baseline_model():
-    return RandomForestClassifier(
-        n_estimators=100,
-        max_depth=None,
-        n_jobs=-1,
-        random_state=42
-    )
